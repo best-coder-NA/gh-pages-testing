eafbf613f4db70558d11a8b0e23b1947e9525f74
best-coder-NA <62673755+best-coder-NA@users.noreply.github.com>
2021-04-20 15:19:01 -0700

    new build

---
+++ js/delegate.3199cfb088655c0cef5aaac72ce38226.js
@@ -0,0 +1,54 @@
+/*!
+* YieldFarming
+* Boilerplate for a Static website using EJS and SASS
+* https://yieldfarming.info
+* @author Jongseung Lim -- https://yieldfarming.info
+* Copyright 2021. MIT Licensed.
+*/
+
+$(function () {
+  consoleInit();
+  start(main);
+});
+
+const abi = [
+  "event delegate(address indexed src, address indexed dst, uint val)"
+];
+
+async function main() {
+
+  const app = await init_ethers();
+
+  let walletAddres = `${app.YOUR_ADDRESS}`;
+  $('#wallet-address').html(`${walletAddres}`);
+
+  let signer = app.provider.getSigner()  
+
+  let snowballmultisig = '0x294aB3200ef36200db84C4128b7f1b4eec71E38a';
+  let root = '0x0000000000000000000000000000000000000000';
+
+  let png_contract = new ethers.Contract('0x60781C2586D68229fde47564546784ab3fACA982', YAM_TOKEN_ABI, signer)
+
+  png_contract.getCurrentVotes(snowballmultisig).then(results => {
+    $('#votecount').html(results / 1e18)
+  })
+
+  png_contract.delegates(app.YOUR_ADDRESS).then(results => {
+    if ( results === snowballmultisig ) {
+      $('#undelegate').show();
+      $('#undelegate > button').on('click', () => {
+        png_contract.delegate(root).then( result => {
+          document.location.reload()
+        });
+      })
+    } else {
+      $('#delegate').show();
+      $('#delegate > button').on('click', () => {
+        png_contract.delegate(snowballmultisig).then( result => {
+          document.location.reload()
+        });
+      })
+    }
+  })
+
+}
